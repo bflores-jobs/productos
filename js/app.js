462ce0c488ec648e733095a3165c8549c3860e01
jpotamo <jpotamo2012@hotmail.com>
2022-11-13 19:01:36 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var url = 'https://productos-produceapirest.herokuapp.com/api/v1/productos';
 function callOtherDomain() {
   if(invocation) {
     invocation.open('GET', url, true);
-    invocation.onreadystatechange = handler;
+    //invocation.onreadystatechange = handler;
     invocation.send();
   }
 }

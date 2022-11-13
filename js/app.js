a6b9751ff9cb9e665cd42095f2ceb713af94c04e
jpotamo <jpotamo2012@hotmail.com>
2022-11-13 20:10:55 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -2,18 +2,12 @@
 //    callOtherDomain()
 //})
 
-var invocation = new XMLHttpRequest();
-var url = 'https://productos-produceapirest.herokuapp.com/api/v1/productos';
-var body = '<?xml version="1.0"?><person><name>Arun</name></person>';
-
-function callOtherDomain(){
-  if(invocation)
-    {
-      invocation.open('POST', url, true);
-      invocation.setRequestHeader('X-PINGOTHER', 'pingpong');
-      invocation.setRequestHeader('Content-Type', 'application/xml');
-      invocation.onreadystatechange = handler;
-      invocation.send(body);
-    }
-    console.log("invocation");
-}
+fetch("https://victim.example/naïve-endpoint", {
+  method: "POST",
+  headers: [
+    ["Content-Type", "application/json"],
+    ["Content-Type", "text/plain"]
+  ],
+  credentials: "include",
+  body: JSON.stringify(exerciseForTheReader)
+});

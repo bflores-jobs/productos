d1689e69cee28470a8f94ebb64e08aecde2ab821
jpotamo <jpotamo2012@hotmail.com>
2022-11-13 19:42:17 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -3,12 +3,16 @@
 //})
 
 var invocation = new XMLHttpRequest();
-var url = 'https://productos-produceapirest.herokuapp.com/api/v1/productos';
+var url = 'http://bar.other/resources/post-here/';
+var body = '<?xml version="1.0"?><person><name>Arun</name></person>';
 
-function callOtherDomain() {
-  if(invocation) {
-    invocation.open('GET', url, true);
-    invocation.onreadystatechange = handler;
-    invocation.send();
-  }
+function callOtherDomain(){
+  if(invocation)
+    {
+      invocation.open('POST', url, true);
+      invocation.setRequestHeader('X-PINGOTHER', 'pingpong');
+      invocation.setRequestHeader('Content-Type', 'application/xml');
+      invocation.onreadystatechange = handler;
+      invocation.send(body);
+    }
 }

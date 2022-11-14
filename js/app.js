4dead11c41b9bb22d195944d8cd55cb0788ecee7
jpotamo <jpotamo2012@hotmail.com>
2022-11-14 04:04:48 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ const pintarCarrito = () => {
         templateCarrito.querySelector('th').textContent = producto.id
         templateCarrito.querySelectorAll('td')[0].textContent = producto.title
         templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
-        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * (producto.cantidad - producto.descuento)
         
         //botones
         templateCarrito.querySelector('.btn-info').dataset.id = producto.id

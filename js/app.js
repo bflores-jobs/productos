35d60fdefb149c6cbb552be907f9095100111a12
jpotamo <jpotamo2012@hotmail.com>
2022-11-14 04:10:44 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -53,8 +53,8 @@ const setCarrito = item => {
         title: item.querySelector('h5').textContent,
         precio: item.querySelector('p').textContent,
         //precio: item.getElementById('precio').textContent,
-        precio: item.querySelector('span').textContent,
-        descuento: item.querySelector('span').textContent,
+        precio: item.querySelectorAll('span')[0].textContent,
+        descuento: item.querySelectorAll('span')[1].textContent,
         id: item.querySelector('button').dataset.id,
         cantidad: 1
     }
